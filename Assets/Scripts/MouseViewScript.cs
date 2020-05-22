3b5d9e4928cd49ca4aa707417511148069dbf10f
Leafurya <46297197+Leafurya@users.noreply.github.com>
2020-05-22 10:11:51 +0900

    1

---
+++ Assets/Scripts/MouseViewScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseViewScript : MonoBehaviour
+{
+    private float sensi = 1.5f;
+    private Transform trans;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Cursor.lockState = CursorLockMode.Locked;
+       // Cursor.visible = false;
+        trans = gameObject.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = (float)Input.GetAxis("Mouse X");
+        float mouseY = (float)Input.GetAxis("Mouse Y");
+        //Debug.Log(string.Format("{0}  {1}",mouseX,mouseY));
+
+        trans.Rotate(-sensi * mouseY, sensi * mouseX, 0f);
+        //Debug.Log(trans.forward);
+        trans.eulerAngles = new Vector3(trans.eulerAngles.x, trans.eulerAngles.y, 0f);
+        //trans.forward = new Vector3(0f, 0f, trans.position.z);
+
+        //trans.forward = new Vector3(trans.eulerAngles.x, trans.eulerAngles.y, 0f); 
+        //trans.rotation = Quaternion.Euler(-sensi * mouseY, sensi * mouseX, 0f);
+    }
+}
---
+++ Assets/Scripts/MoveScript.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.U2D;
+using UnityEngine;
+
+public class MoveScript : MonoBehaviour
+{
+    private Rigidbody rigi;
+    private Transform trans;
+    private float speed=1.2f;
+    private float jump = 2.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigi = gameObject.GetComponent<Rigidbody>();
+        trans = gameObject.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+    private void Move()
+    {
+        float yVel = rigi.velocity.y;
+        float sxVel = 0f, szVel = 0f;
+        float fxVel = 0f, fzVel = 0f;
+        
+        float tvec = (Math.Abs(trans.forward.x) + Math.Abs(trans.forward.z)) > 0 ? (Math.Abs(trans.forward.x) + Math.Abs(trans.forward.z)) : 1;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            fxVel = trans.forward.x / tvec;
+            fzVel = trans.forward.z / tvec;
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            fxVel = -trans.forward.x / tvec;
+            fzVel = -trans.forward.z / tvec;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            sxVel = -trans.forward.z / tvec;
+            szVel = trans.forward.x / tvec;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            sxVel = trans.forward.z / tvec;
+            szVel = -trans.forward.x / tvec;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log(rigi.velocity.y * yVel);
+            yVel = jump;
+        }
+        rigi.velocity = new Vector3((fxVel + sxVel)*speed, yVel, (fzVel + szVel)*speed);
+        //Debug.Log(trans.forward);
+        //Debug.Log(string.Format("{0} {1}", fxVel, fzVel));
+        //rigi.velocity = new Vector3(xVel, yVel, zVel);
+        //Debug.Log(rigi.velocity);
+        //rigi.velocity = Vector3.zero;
+    }
+}
